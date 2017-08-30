fe9a163ef6e75d53b41320cb0ff0a7c0d01c06f4
alla3118 <alla3118@ELSTJEPERS-76>
2017-08-30 12:21:45 +0200

    Add project files.

---
+++ WeatherApp/WeatherApp/DataService.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Net.Http;
+
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherApp
+{
+    class DataService
+    {
+        public static async Task<dynamic> getDataFromService(string pQueryString)
+        {
+            HttpClient client = new HttpClient();
+            var response = await client.GetAsync(pQueryString);
+            dynamic data = null;
+            if (response != null)
+            {
+                string json = response.Content.ReadAsStringAsync().Result;
+                data = JsonConvert.DeserializeObject(json);
+            }
+            return data;
+        }
+    }
+}
