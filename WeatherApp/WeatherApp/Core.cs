9ead38b69e29ca956d27537c92bf0870bb975abe
alla3118 <alla3118@hedskole.dk>
2017-09-01 12:30:25 +0200

    TUT 1 bottom

---
+++ WeatherApp/WeatherApp/Core.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WeatherApp;
+
+namespace WeatherApp
+{
+    class Core
+    {
+        public static async Task<Weather> GetWeather(string pZipCode)
+        {
+            string key = "86e7fe12d67265a4bb37cc0e131f35bf";
+            string queryString = "http://api.openweathermap.org/data/2.5/weather?zip="
+                + pZipCode + "us&appid=" + key + "&units=imperial";
+            if (key != "86e7fe12d67265a4bb37cc0e131f35bf")
+            {
+                throw new ArgumentException("You must obtain an API key from" +
+                    "openweathermap.org/appid and save it in the'key' variable");
+            }
+            dynamic results = await DataService.getDataFromService(queryString).ConfigureAwait(false);
+            if (results["weather"] != null)
+            {
+                Weather weather = new Weather();
+                weather.Title = (string)results["name"];
+                weather.Temperature = (string)results["main"]["temp"] + " F";
+                weather.Wind = (string)results["wind"]["speed"] + " mph";
+                weather.Humidity = (string)results["main"]["humidity"] + " %";
+                weather.Visibility = (string)results["weather"][0]["main"];
+
+                DateTime time = new System.DateTime(1970, 1, 1, 0, 0, 0, 0);
+                DateTime sunrise = time.AddSeconds((double)results["sys"]["sunrise"]);
+                DateTime sunset = time.AddSeconds((double)results["sys"]["sunset"]);
+                weather.Sunrise = sunrise.ToString() + " UTC";
+                weather.Sunset = sunset.ToString() + " UTC";
+                return weather;
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
--- WeatherApp/WeatherApp/MainPage.xaml.cs
+++ WeatherApp/WeatherApp/MainPage.xaml.cs
@@ -12,6 +12,27 @@ namespace WeatherApp
         public MainPage()
         {
             InitializeComponent();
+            btnGetWeather.Clicked += btnGetWeather_Click;
+        }
+        private async void btnGetWeather_Click(object sender, EventArgs e)
+        {
+            if(String.IsNullOrEmpty(edtZipCode.Text))
+            {
+                Weather weather = await Core.GetWeather(edtZipCode.Text);
+
+                if (weather != null)
+                {
+                    txtLocation.Text = weather.Title;
+                    txtTemperature.Text = weather.Temperature;
+                    txtWind.Text = weather.Wind;
+                    txtVisibility.Text = weather.Visibility;
+                    txtHumidity.Text = weather.Humidity;
+                    txtSunrise.Text = weather.Sunrise;
+                    txtSunset.Text = weather.Sunset;
+
+                    btnGetWeather.Text = "Search Again";
+                }
+            }
         }
     }
 }
